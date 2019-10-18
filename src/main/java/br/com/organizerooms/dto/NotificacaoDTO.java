cfa46100e4467aefacacd65faba9745d60ca5c1b
LucasJansen <lucasrainoldojansen@gmail.com>
2019-10-18 20:57:25 -0300

    Commit com projeto correto

---
+++ src/main/java/br/com/organizerooms/dto/NotificacaoDTO.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.organizerooms.dto;
+
+
+import br.com.organizerooms.models.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.EntityListeners;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ *
+ * @author Felipe
+ */
+
+public class NotificacaoDTO {
+    
+    private Long notId;
+
+    private String notDescricao;
+    
+    private boolean notAtiva;
+    
+    private Date notDtCadastro;
+
+    private Date notDtAtualizacao;
+  
+    private Pessoa notPesAtualizacao;
+
+    private Pessoa pessoaId;
+
+    public NotificacaoDTO(Long notId, String notDescricao, boolean notAtiva, Date notDtCadastro, Date notDtAtualizacao, Pessoa notPesAtualizacao, Pessoa pessoaId) {
+        this.notId = notId;
+        this.notDescricao = notDescricao;
+        this.notAtiva = notAtiva;
+        this.notDtCadastro = notDtCadastro;
+        this.notDtAtualizacao = notDtAtualizacao;
+        this.notPesAtualizacao = notPesAtualizacao;
+        this.pessoaId = pessoaId;
+    }
+    
+    public NotificacaoDTO(Notificacao notificacao) {
+        this.notId = notificacao.getNotId();
+        this.notDescricao = notificacao.getNotDescricao();
+        this.notAtiva = notificacao.isNotAtiva();
+        this.notDtCadastro = notificacao.getNotDtCadastro();
+        this.notDtAtualizacao = Calendar.getInstance().getTime();
+        this.notPesAtualizacao = notificacao.getNotPesAtualizacao();
+        this.pessoaId = notificacao.getPessoaId();
+    }
+
+    public Long getNotId() {
+        return notId;
+    }
+
+    public void setNotId(Long notId) {
+        this.notId = notId;
+    }
+
+    public String getNotDescricao() {
+        return notDescricao;
+    }
+
+    public void setNotDescricao(String notDescricao) {
+        this.notDescricao = notDescricao;
+    }
+
+    public boolean isNotAtiva() {
+        return notAtiva;
+    }
+
+    public void setNotAtiva(boolean notAtiva) {
+        this.notAtiva = notAtiva;
+    }
+
+    public Date getNotDtCadastro() {
+        return notDtCadastro;
+    }
+
+    public void setNotDtCadastro(Date notDtCadastro) {
+        this.notDtCadastro = notDtCadastro;
+    }
+
+    public Date getNotDtAtualizacao() {
+        return notDtAtualizacao;
+    }
+
+    public void setNotDtAtualizacao(Date notDtAtualizacao) {
+        this.notDtAtualizacao = notDtAtualizacao;
+    }
+
+    public Pessoa getNotPesAtualizacao() {
+        return notPesAtualizacao;
+    }
+
+    public void setNotPesAtualizacao(Pessoa notPesAtualizacao) {
+        this.notPesAtualizacao = notPesAtualizacao;
+    }
+
+    public Pessoa getPessoaId() {
+        return pessoaId;
+    }
+
+    public void setPessoaId(Pessoa pessoaId) {
+        this.pessoaId = pessoaId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + Objects.hashCode(this.notId);
+        hash = 83 * hash + Objects.hashCode(this.notDescricao);
+        hash = 83 * hash + (this.notAtiva ? 1 : 0);
+        hash = 83 * hash + Objects.hashCode(this.notDtCadastro);
+        hash = 83 * hash + Objects.hashCode(this.notDtAtualizacao);
+        hash = 83 * hash + Objects.hashCode(this.notPesAtualizacao);
+        hash = 83 * hash + Objects.hashCode(this.pessoaId);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final NotificacaoDTO other = (NotificacaoDTO) obj;
+        if (this.notAtiva != other.notAtiva) {
+            return false;
+        }
+        if (!Objects.equals(this.notDescricao, other.notDescricao)) {
+            return false;
+        }
+        if (!Objects.equals(this.notId, other.notId)) {
+            return false;
+        }
+        if (!Objects.equals(this.notDtCadastro, other.notDtCadastro)) {
+            return false;
+        }
+        if (!Objects.equals(this.notDtAtualizacao, other.notDtAtualizacao)) {
+            return false;
+        }
+        if (!Objects.equals(this.notPesAtualizacao, other.notPesAtualizacao)) {
+            return false;
+        }
+        if (!Objects.equals(this.pessoaId, other.pessoaId)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
