4ff39cad9fd8c661cc249cce57cdac2f29319bfa
ederjeandias <eder.jean.dias@philips.com>
2019-10-25 21:25:18 -0300

    Trazer agendamentos pelo id da Sala e Pessoa

--- src/main/java/br/com/organizerooms/repositorios/AgendamentoRepository.java
+++ src/main/java/br/com/organizerooms/repositorios/AgendamentoRepository.java
@@ -1,6 +1,8 @@
 package br.com.organizerooms.repositorios;
 
 import br.com.organizerooms.models.Agendamento;
+import br.com.organizerooms.models.Pessoa;
+import br.com.organizerooms.models.Sala;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,7 +16,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true )
 public interface AgendamentoRepository extends JpaRepository <Agendamento, Long >{
    
-    public List<Agendamento> findAllByOrderByAgeDescricao(); 
+    public List<Agendamento> findAllByOrderByAgeDescricao();
+    
+    public List<Agendamento> findByPesIdResponsavel(Pessoa pessoa);
+    
+    public List<Agendamento> findBySalaId(Sala sala);
     
 }
 
